5d936543d8cc4e66c347a913bfc173aee158dc4f
SIE-KYIN <kjsl4tw@naver.com>
2022-07-11 18:26:37 +0900

    스택 예제 완료

---
+++ stack/README.md
@@ -0,0 +1,1 @@
+[상세보기](https://sie-kyin.github.io/swiftui/2022/07/11/stack_ex/)
--- stack/stack/ContentView.swift
+++ stack/stack/ContentView.swift
@@ -7,10 +7,55 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+//    var colors : [(String, Color)] = [("Black", .black), ("Red", .red), ("Green", .green), ("Blue", .blue)] 딕셔너리 접근을 어캐하지
+    var colors : [Color] = [.black, .red, .green, .blue]
+    var colornames : [String] = ["black", "red", "green", "blue"]
+    @State private var colorIndex = 0
+    @State private var sliderValue : Double = 0.0
+    @State private var text : String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(text)
+                .font(.largeTitle)
+                .foregroundColor(self.colors[self.colorIndex])
+                .rotationEffect(.degrees(self.sliderValue))
+                .padding(.bottom, 100)
+            
+            HStack {
+                Spacer(minLength: 20)
+                Text("0")
+                Spacer(minLength: 20)
+                Slider(value: $sliderValue, in: 0...360, step:1)
+                    .accentColor(.red)
+                Spacer(minLength: 20)
+                Text("360")
+                Spacer(minLength: 20)
+            }
+            Text("\(sliderValue)")
+            Text("\(sliderValue, specifier: "%.2f")")
+            Text("\(Int(sliderValue))")
+                .padding()
+            
+            HStack {
+                Text("Text")
+                    .frame(width: 100, height: 30, alignment: .center)
+                    .background(.cyan)
+                    .foregroundColor(.white)
+                TextField("Write some Text", text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            
+            Picker(selection: $colorIndex, label: Text("Text Color")) {
+                ForEach (0 ..< colornames.count, id:\.self) {
+                    Text(self.colornames[$0])
+                }
+            }
+            .padding(.top, 100)
+            
+        }
     }
 }
 
