53ddc41616d2468643b36a315de484286ddd1e15
SIE-KYIN <kjsl4tw@naver.com>
2022-07-11 15:25:04 +0900

    stack 예제 폴더 생성

---
+++ stack/stack/stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackApp.swift
+//  stack
+//
+//  Created by Kyu jin Lee on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
